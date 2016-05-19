30b58c81dad46fc9224ffa21fe82cffe816ec4c1
Damien Dosimont <damien.dosimont@bsc.es>
2016-05-19 14:54:58 +0200

    updating readme

--- src/RRI-bin/argumentmanager.cpp
+++ src/RRI-bin/argumentmanager.cpp
@@ -50,7 +50,7 @@ void ArgumentManager::printUsage()
     qDebug().nospace()<<"\t\t\t"<<"-ts --timeslices [integer]: "<<"timeslice number used to discretize the time period ("<<TSNUMBER<<" by default)";
     qDebug().nospace()<<"\t\t\t"<<"-th --threshold [float]: "<<"minimal distance between two parameters p ("<<THRESHOLD<<" by default)";
     qDebug().nospace()<<"\t\t\t"<<"-mp --minprop [float]: "<<"routine minimal proportion ("<<MINPROP<<" by default)";
-    qDebug().nospace()<<"\t\t\t"<<"-r --region [string]: "<<"apply rri on a region; may be used several times to involve several regions";
+    qDebug().nospace()<<"\t\t\t"<<"-r --region [string]: "<<"apply rri on a single region; can be used several times to involve several regions";
     //qDebug().nospace()<<"\t\t\t"<<"-nv --novoid: "<<"dynamically reduce the timeslice number to prevent empty timeslices";
 }
 
